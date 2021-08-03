92e09df8875a48302cc8d06f44be0f3367051d89
dmssantos <dms.santos_@hotmail.com>
2021-08-03 12:23:23 -0300

    update

--- naruto-quotes-client/src/components/globalStyle/GlobalStyle.js
+++ naruto-quotes-client/src/components/globalStyle/GlobalStyle.js
@@ -1,7 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
+import { bgImg } from '../../images';
 
 export const GlobalStyle = createGlobalStyle`
   body {
+    background: url(${bgImg}) center no-repeat;
+    background-size: cover;
     color: #332c36;
     padding: 0;
     margin: 0;
--- naruto-quotes-client/src/components/quotes/Quotes.js
+++ naruto-quotes-client/src/components/quotes/Quotes.js
@@ -1,9 +1,34 @@
+import styled from 'styled-components';
+import { string } from 'prop-types';
+
+
+Quotes.prototype = {
+  quote: string,
+  speaker: string
+}
 export const Quotes = ({ quote, speaker }) => {
   return (
-    <>
-      <p>{quote}</p>
-      <p> - {speaker}</p>
+    <Wrapper>
+      <Quote>{quote}</Quote>
+      <Speaker> - {speaker}</Speaker>
       <button>Quote No Jutsu</button>
-    </>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Quote = styled.p`
+  font-size: 2em;
+  margin: 0;
+`;
+
+const Speaker = styled(Quote)`
+  text-align: right;
+  margin-bottom: 50px;
+`;
